5be9df758dbfec43972e93931ea4cc78901ddf67
Rubalpreet kaur <84393565+Rubalpreet832@users.noreply.github.com>
2021-07-25 11:26:47 +0530

    Delete convertToNumber.js

--- convertToNumber.js
+++
@@ -1,8 +0,0 @@
-let input = process.argv.slice(2);
-console.log(input);
-let word1 = input[0];
-let word2 = input[1];
-
-const finalWordCount = word1.length + word2.length;
-console.log(finalWordCount);
-
